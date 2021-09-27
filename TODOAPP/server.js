8afaae395c154bb748d625e23481ed009083eddd
이태성 <dlxotjde@gmail.com>
2021-09-27 21:51:26 +0900

    Add cr

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -3,41 +3,52 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended : true}))
+app.set('view engine', 'ejs')
 
 
+// /list로 get요청 보여주기
+app.get('/list', (요청, 응답) => {
+    // 디비에 저장된 post라는 collection안의 특정 데이터들을 꺼내중셍용
+    db.collection('post').find().toArray((에러, 결과) => {
+        console.log(결과)
+        응답.render('list.ejs', { posts : 결과 })
+    })
+    // (function(에러, 결과) {
+    //     // 메타데이터 도 꽂아서 온다 find까지
+    //     console.log(결과)
+    // })
+})
+
+var db
 MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.geuvl.mongodb.net/PJT0?retryWrites=true&w=majority', function(에러, client){
     if (에러) return console.log(에러);
     //서버띄우는 코드 여기로 옮기기
+    
+    // todoapp이라는 db(폴더)에 연결좀요
+    db = client.db('todoapp')
+        
     app.listen('8080', function(){
-            console.log('listening on 8080')
-        });
-    })
+        console.log('listening on 8080')
+    });
+        
+        
+})
+app.post('/add', function(요청, 응답) {
+    응답.send('전송완료')
+    console.log('됨')
+    db.collection('post').insertOne( { 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
+        console.log('저장완료')
+    });
+})
 
-// app.listen(8080, function(){
-//     console.log('listening on 8080') 
-// })
 
-app.get('/pet', function(요청, 응답) {
-    응답.send('펫 쇼핑할 수 있는 페이지입니다')
-    
-})
-app.get('/beauty', function(요청, 응답) {
-    응답.send('뷰티 할 수 있는 페이지입니다')
-    
-})
-app.get('/', function(요청, 응답) {
-    응답.sendFile(__dirname + '/index.html')
-    
-})
 app.get('/write', function(요청, 응답) {
     응답.sendFile(__dirname + '/write.html')
     
 })
 
-app.post('/add', (요청, 응답) => {
-    응답.send('전송완료')
-    console.log(요청.body.title)
-    console.log(요청.body.date)
-    console.log(요청.body)
-    //db에 저장해주세요
-})
+
+// 어떤 사람이 /add 라는 경로로 post 요청을 하면,
+// 데이터 2개(날짜, 제목)를 보내준다.
+
+
